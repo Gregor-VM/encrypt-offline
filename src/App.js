b87dfed5378626a31977036b9b1435c73ed9a864
GregorVM <gregorvm10@gmail.com>
2021-02-11 13:30:44 -0400

    first version upload to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Sustitución Random</h1>
+    </header>
+  );
+}
+
+function GenerateButton(params) {
+  return (<div className="containerButton">
+  <button>Generar</button>
+  <img src="#"></img>
+</div>);
+}
+
+/*
+const abc = 'qwertyuiopasdfghjklñzxcvbnm';
+const code = '1234567890*!"#$%&/()=?¡¿qw';
+const text = ' alsjflasjf ';
+
+text.split('').map((e, i) => abc.indexOf(e.toLowerCase()) == -1 ? ' ' : code.split('')[ abc.indexOf(e.toLowerCase())] ).join('');
+ */
+
+class GetBox extends React.Component{
+
+  render(){
+    return (
+      <div className="containerBox">
+        <GenerateButton></GenerateButton>
+        <textarea className="generateTextArea"></textarea>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <GetBox></GetBox>
     </div>
+
   );
 }
 
